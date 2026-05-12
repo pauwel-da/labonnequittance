34e04f57af419342172ffc5aea47e3d4f8e057c1
Pauwel <pauwel@hotmail.fr>
2026-05-12 23:58:33 +0200

    u

--- src/components/ReadingProgress.tsx
+++ src/components/ReadingProgress.tsx
@@ -16,7 +16,7 @@ export default function ReadingProgress() {
   }, [])
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-[100] h-1 bg-gray-100">
+    <div className="absolute bottom-0 left-0 right-0 h-[3px] bg-gray-100">
       <div
         className="h-full bg-[#008020] transition-[width] duration-75 ease-out"
         style={{ width: `${progress}%` }}
